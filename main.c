2b68b7d2b4ec9aeefd85a807afd5a616861d09b5
ctronp <carlostp7@uc.cl>
2022-07-13 14:38:47 -0400

    trying to add Google tests

--- main.c
+++ main.c
@@ -1,40 +1,40 @@
 #define T int
-#include "include/generic_vector.h"
+#include "src/generic_vector.h"
 #undef T
 
 #define T unsigned
-#include "include/generic_vector.h"
+#include "src/generic_vector.h"
 #undef T
 
 #define T int
-#include "include/generic_ll.h"
+#include "src/generic_ll.h"
 #undef T
 
 #define T unsigned
-#include "include/generic_ll.h"
+#include "src/generic_ll.h"
 #undef T
 
 #define T int
-#include "include/generic_dll.h"
+#include "src/generic_dll.h"
 #undef T
 
 #define T unsigned
-#include "include/generic_dll.h"
+#include "src/generic_dll.h"
 #undef T
 
 #define T int
-#include "include/generic_fifo.h"
+#include "src/generic_fifo.h"
 #undef T
 
 #define T unsigned
-#include "include/generic_fifo.h"
+#include "src/generic_fifo.h"
 #undef T
 
 size_t test_hash(int i) { return (size_t)i; }
 
 #define T int
 #define HASH_F test_hash
-#include "include/generic_set.h"
+#include "src/generic_set.h"
 #undef T
 #undef HASH_F
 
@@ -42,7 +42,7 @@ size_t test_hash2(int i) { return (size_t)i; }
 
 #define T unsigned
 #define HASH_F test_hash2
-#include "include/generic_set.h"
+#include "src/generic_set.h"
 #undef T
 
 #include <stdio.h>
