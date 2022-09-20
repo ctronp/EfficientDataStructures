bbd5b9bbdf6a7a6df0783b082148cd8a0f7ba825
ctronp <carlostp7@uc.cl>
2022-09-20 17:35:40 -0300

    testing

--- tests/fifo/memory_testing.c
+++ tests/fifo/memory_testing.c
@@ -4,7 +4,7 @@
 
 #include "../MCT/MCT.h"
 
-START_TESTING(NULL) { printf("start testing Linked Lists\n"); }
+START_TESTING(NULL) { printf("start testing FIFO\n"); }
 
 TEST("create_delete", "create and delete a fifo") {
   fifo_int *f = new_fifo_int();
@@ -33,7 +33,7 @@ TEST("various_append", "test appending multiple times") {
   free(l);
 }
 
-TEST("2 lists", "test appending and popping from 2 lists") {
+TEST("2 lists", "test appending and popping from 2 fifo") {
   fifo_int *l1 = new_fifo_int();
   fifo_int *l2 = new_fifo_int();
   ASSERT_NE_PTR(NULL, l1);
@@ -45,7 +45,7 @@ TEST("2 lists", "test appending and popping from 2 lists") {
     append_fifo_int(l2, i);
   }
 
-  for (int i = 0; i <1000 ; i++) {
+  for (int i = 0; i < 1000; i++) {
     ASSERT_EQ_INT(pop_left_fifo_int(l2), pop_left_fifo_int(l1))
   }
 
@@ -55,7 +55,7 @@ TEST("2 lists", "test appending and popping from 2 lists") {
   free(l2);
 }
 
-TEST("with capacity", "test appending and popping from a list with capacity") {
+TEST("with capacity", "test appending and popping from a fifo with capacity") {
   fifo_int *l1 = new_fifo_with_capacity_int(1000);
   fifo_int *l2 = new_fifo_with_capacity_int(1000);
 
@@ -81,153 +81,67 @@ TEST("with capacity", "test appending and popping from a list with capacity") {
   free(l1);
   free(l2);
 }
-//
-//TEST("init list", "test initializing a list") {
-//  list_int ll, *l1 = &ll;
-//  list_int *l2 = new_list_with_capacity_int(1000);
-//
-//  ASSERT_NE_PTR(NULL, l1)
-//  ASSERT_NE_PTR(NULL, l2)
-//
-//  init_list_int(l1);
-//
-//  for (int i = 0; i < 1000; i++) {
-//    append_left_list_int(l1, i);
-//  }
-//
-//  for (int i = 0; i < 1000; i++) {
-//    append_left_list_int(l2, i);
-//  }
-//
-//  for (int i = 999; i >= 0; i--) {
-//    int t1 = pop_left_list_int(l1);
-//    ASSERT_EQ_INT(t1, pop_left_list_int(l2))
-//    ASSERT_EQ_INT(i, t1)
-//  }
-//
-//  del_list_int(l1);
-//  del_list_int(l2);
-//  free(l2);
-//}
-//
-//TEST("clean list", "test cleaning a list") {
-//  list_int *l1 = new_list_int();
-//  list_int *l2 = new_list_int();
-//  ASSERT_NE_PTR(NULL, l1);
-//  ASSERT_NE_PTR(NULL, l2);
-//  for (int i = 0; i < 1000; i++) {
-//    append_left_list_int(l1, i);
-//  }
-//  for (int i = 0; i < 1000; i++) {
-//    append_left_list_int(l2, i);
-//  }
-//
-//  clean_list_int(l1);
-//  clean_list_int(l2);
-//  ASSERT_EQ_UNS(0, l1->size)
-//  ASSERT_EQ_UNS(0, l2->size)
-//
-//  for (int i = 0; i < 1000; i++) {
-//    append_left_list_int(l1, i);
-//  }
-//  for (int i = 0; i < 1000; i++) {
-//    append_left_list_int(l2, i);
-//  }
-//
-//  for (int i = 0; i < 1000; i++) {
-//    ASSERT_EQ_INT(pop_left_list_int(l1), pop_left_list_int(l2))
-//  }
-//
-//  del_list_int(l1);
-//  del_list_int(l2);
-//  free(l1);
-//  free(l2);
-//}
-//
-//TEST("fit list", "test fitting a list") {
-//  list_int *l1 = new_list_int();
-//  list_int *l2 = new_list_int();
-//  ASSERT_NE_PTR(NULL, l1);
-//  ASSERT_NE_PTR(NULL, l2);
-//  for (int i = 0; i < 1000; i++) {
-//    append_left_list_int(l1, i);
-//  }
-//  for (int i = 0; i < 1000; i++) {
-//    append_left_list_int(l2, i);
-//  }
-//
-//  fit_list_int(l1);
-//  fit_list_int(l2);
-//  ASSERT_EQ_UNS(1000, l1->size)
-//  ASSERT_EQ_UNS(1000, l2->size)
-//  ASSERT_EQ_UNS(l1->lifo_free_capacity, 0)
-//  ASSERT_EQ_UNS(l2->lifo_free_capacity, 0)
-//
-//  for (int i = 0; i < 1000; i++) {
-//    ASSERT_EQ_INT(pop_left_list_int(l1), pop_left_list_int(l2))
-//  }
-//
-//  fit_list_int(l1);
-//  fit_list_int(l2);
-//  ASSERT_EQ_UNS(0, l1->size)
-//  ASSERT_EQ_UNS(0, l2->size)
-//
-//  del_list_int(l1);
-//  del_list_int(l2);
-//  free(l1);
-//  free(l2);
-//}
-//
-//TEST("pop with index", "test popping from a list with index") {
-//  list_int *l1 = new_list_int();
-//  list_int *l2 = new_list_int();
-//  ASSERT_NE_PTR(NULL, l1);
-//  ASSERT_NE_PTR(NULL, l2);
-//  for (int i = 0; i < 1000; i++) {
-//    append_left_list_int(l1, i);
-//  }
-//  for (int i = 0; i < 1000; i++) {
-//    append_left_list_int(l2, i);
-//  }
-//
-//  for (int i = 0; i < 1000; i++) {
-//    ASSERT_EQ_INT(i, pop_with_index_int(l1, 999 - i))
-//  }
-//
-//  for (int i = 0; i < 1000; i++) {
-//    ASSERT_EQ_INT(999 - i, pop_with_index_int(l2, 0))
-//  }
-//
-//  del_list_int(l1);
-//  del_list_int(l2);
-//  free(l1);
-//  free(l2);
-//}
-//
-//TEST("pop with pos", "test popping from a list with pos") {
-//  list_int *l1 = new_list_int();
-//  list_int *l2 = new_list_int();
-//  ASSERT_NE_PTR(NULL, l1);
-//  ASSERT_NE_PTR(NULL, l2);
-//  for (int i = 0; i < 1000; i++) {
-//    append_left_list_int(l1, i);
-//  }
-//  for (int i = 0; i < 1000; i++) {
-//    append_left_list_int(l2, i);
-//  }
-//
-//  for (int i = 0; i < 1000; i++) {
-//    ASSERT_EQ_INT(999 - i, pop_with_pos_int(l1, 999 - i))
-//  }
-//
-//  for (int i = 0; i < 1000; i++) {
-//    ASSERT_EQ_INT(i, pop_with_pos_int(l2, i))
-//  }
-//
-//  del_list_int(l1);
-//  del_list_int(l2);
-//  free(l1);
-//  free(l2);
-//}
+
+TEST("init list", "test initializing a list") {
+  fifo_int fifo, *f1 = &fifo;
+  fifo_int *f2 = new_fifo_with_capacity_int(1000);
+
+  ASSERT_NE_PTR(NULL, f1)
+  ASSERT_NE_PTR(NULL, f2)
+
+  init_fifo_int(f1);
+
+  for (int i = 0; i < 1000; i++) {
+    append_fifo_int(f1, i);
+  }
+
+  for (int i = 0; i < 1000; i++) {
+    append_fifo_int(f2, i);
+  }
+
+  for (int i = 0; i < 1000; i++) {
+    int t1 = pop_left_fifo_int(f1);
+    ASSERT_EQ_INT(t1, pop_left_fifo_int(f2))
+    ASSERT_EQ_INT(i, t1)
+  }
+
+  del_fifo_int(f1);
+  del_fifo_int(f2);
+  free(f2);
+}
+
+TEST("clean list", "test cleaning a list") {
+  fifo_int *f1 = new_fifo_int();
+  fifo_int *f2 = new_fifo_int();
+  ASSERT_NE_PTR(NULL, f1);
+  ASSERT_NE_PTR(NULL, f2);
+  for (int i = 0; i < 1000; i++) {
+    append_fifo_int(f1, i);
+  }
+  for (int i = 0; i < 1000; i++) {
+    append_fifo_int(f2, i);
+  }
+
+  clean_fifo_int(f1);
+  clean_fifo_int(f2);
+  ASSERT_EQ_UNS(0, f1->size)
+  ASSERT_EQ_UNS(0, f2->size)
+
+  for (int i = 0; i < 1000; i++) {
+    append_fifo_int(f1, i);
+  }
+  for (int i = 0; i < 1000; i++) {
+    append_fifo_int(f2, i);
+  }
+
+  for (int i = 0; i < 1000; i++) {
+    ASSERT_EQ_INT(pop_left_fifo_int(f1), pop_left_fifo_int(f2))
+  }
+
+  del_fifo_int(f1);
+  del_fifo_int(f2);
+  free(f1);
+  free(f2);
+}
 
 END_TESTING
